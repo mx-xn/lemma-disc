fa68efe73bbca0abf567a41275d15d5baccda6a5
Maxine Xin <maxin.xin.sds@gmail.com>
2026-07-07 20:11:50 +0000

    delete caceh

--- exp/eval/sanity/output/sorts_mathlib/without_lemmas/cache/20260625_031558_386736_expB_Permut.symm/scratch.lean
+++
@@ -1,9 +0,0 @@
-import LeanSrc.Sorts_Mathlib
-
-theorem expB_Permut.symm [DecidableEq T] {l m : List T} (p : Permut l m) : Permut m l := by
-  intro t
-  symm
-  exact p t
-
-
-/- lean_check result: ok -/
